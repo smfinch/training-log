685f489a25ee3f7fa6365605148216e89b68eab7
Scott Finch <scott.m.finch@gmail.com>
2018-02-24 09:05:48 -0500

    Added tables to Activities component

--- client/src/Activities.js
+++ client/src/Activities.js
@@ -5,6 +5,38 @@ class Activities extends Component {
     return (
       <div className="container">
         <h1>Activies</h1>
+        <div className="row">
+          <div className="col s12 m12">
+            <div className="card-panel">
+              <table className="highlight">
+                <thead>
+                  <tr>
+                      <th>Name</th>
+                      <th className="center">Type</th>
+                      <th className="right">Distance</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Easy Run</td>
+                    <td className="center">RR</td>
+                    <td className="right">5</td>
+                  </tr>
+                  <tr>
+                    <td>Riverside Tempo</td>
+                    <td className="center">TR</td>
+                    <td className="right">12</td>
+                  </tr>
+                  <tr>
+                    <td>10X400</td>
+                    <td className="center">RI</td>
+                    <td className="right">10</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
